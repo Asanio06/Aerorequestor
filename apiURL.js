add82ec5d4db0b49658bb2cd7a688a91c15b859b
Lansana <lansana.diomande@epfedu.fr>
2021-05-12 20:51:18 +0200

    Adaptation de l'extension pour communiquer avec une API node

---
+++ apiURL.js
@@ -0,0 +1,1 @@
+const urlApi = 'http://localhost:8000/api';
--- metar_charts.js
+++ metar_charts.js
@@ -1,36 +1,64 @@
+const urlApi = 'http://localhost:8000/api';
+
 var button_Request= document.getElementById('Request')
 button_Request.addEventListener('click',getMetar)
 var button_charts = document.getElementById('Open_charts')
 button_charts.addEventListener('click',open_link_of_sia)
+var link_vfr_charts = document.getElementById('lien_chart_vfr')
+link_vfr_charts.addEventListener('click',open_vfr_chart)
 
 $.ajax({
     type: 'GET',
-    url: 'http://asanio.alwaysdata.net/index.php',
-    dataType: 'html',
-    data : {request :'airport_list' },
+    url: `${urlApi}/airport/`,
+    dataType: 'json',
     success: function (result) {
-      document.getElementById('content_for_datalist').innerHTML = result
+        const listAirport = result.allAirport;
+        console.log(listAirport[0])
+        var options = '';
+
+        listAirport.forEach((airport)=>{
+            options += `<option value="${airport.ident}"> ${airport.name} </option>`;
+
+        })
+      document.getElementById('list_airport').innerHTML = options;
     }
   });
 
 
 function getMetar(){
 
-    var airport = document.getElementById('ICAO_airport').value
-    airport = airport.toUpperCase()
+    var airportICAO = document.getElementById('ICAO_airport').value
+    airportICAO = airportICAO.toUpperCase()
 
     $.ajax({
         type: 'GET',
-        url: 'http://asanio.alwaysdata.net/index.php',
-        data: { request: "metar_of_airport", airport: airport },
+        url: `${urlApi}/weather/metar/${airportICAO}`,
         dataType: 'json',
         success: function (result) {
-          document.getElementById('zone_display_metar').style.visibility = 'visible'
-          document.getElementById('Metar_display').innerText = result['metar']
-          document.getElementById('Airport_Advise').innerText = result['advise']
+          document.getElementById('zone_display_metar').style.visibility = 'visible';
+          console.log(result)
+          document.getElementById('Metar_display').innerText = result.weatherData.raw_text;
+            const flightCategory = result.weatherData.flight_category;
+            let advise;
+            if(flightCategory){
+
+                if(flightCategory == 'VFR'){
+                    advise = "You can do all type of flight" ;
+
+                }else if(flightCategory == 'MVFR'){
+                    advise = "You can do special vfr and IFR";
+                }else{
+                    advise = "You can also do IFR flight";
+                }
+
+            }else{
+                advise = 'No advise for this airport';
+            }
+          document.getElementById('Airport_Advise').innerText = advise;
           
-          //document.getElementById('advise_content').style.visibility = 'visible'
-          get_datalist_ifr_charts_of_airport(airport)
+          document.getElementById('Airport_Advise').style.visibility = 'visible';
+          get_datalist_ifr_charts_of_airport(airportICAO)
+          //get_vfr_link_of_chart(airportICAO)
         }
       });
 
@@ -40,19 +68,80 @@ function get_datalist_ifr_charts_of_airport(icao_airport){
 
   $.ajax({
     type: 'GET',
-    url: 'http://asanio.alwaysdata.net/index.php',
-    data: { request: "list_charts_of_airport", airport: icao_airport },
-    dataType: 'html',
+    url: `${urlApi}/chart/airport/${icao_airport}`,
+    dataType: 'json',
     statusCode: {
-      204: function() {
-        document.getElementById('zone_ifr_charts').style.visibility = 'hidden'
+      400: function() {
+        document.getElementById('zone_ifr_charts').style.visibility = 'hidden';
       }
     },
     success: function (result) {
       if(result){
-        document.getElementById('zone_ifr_charts').style.visibility = 'visible'
+        document.getElementById('zone_ifr_charts').style.visibility = 'visible';
         //document.getElementById('list_ifr_charts').value = ''
-        document.getElementById('content_for_datalist_list_ifr_chart').innerHTML = result
+          const listChart = result.listCarteAerport;
+          console.log(result)
+
+          var options = '';
+
+          listChart.forEach((chart)=>{
+              options += `<option id="${chart.url}" value="${chart.Chart_name} "> </option>`;
+
+          })
+        document.getElementById('list_ifr_chart').innerHTML = options;
+      }
+  
+    }
+  });
+
+}
+
+function get_vfr_link_of_chart(icao_airport){
+
+
+  $.ajax({
+    type: 'GET',
+    url: 'http://asanio.alwaysdata.net/index.php',
+    data: { request: "get_url_of_vfr_chart", airport: icao_airport },
+    dataType: 'json',
+    statusCode: {
+      204: function() {
+        document.getElementById('zone_vfr_charts').style.visibility = 'hidden'
+      }
+    },
+    success: function (result) {
+      if(result){
+
+        document.getElementById('zone_vfr_charts').style.visibility = 'visible'
+        lien = result
+        document.getElementById('lien_chart_vfr').innerHTML = '<a href="'+ lien +' target="_blank"">LIEN</a>'
+        
+      }
+  
+    }
+  });
+
+}
+
+function open_vfr_chart(){
+  var airport = document.getElementById('ICAO_airport').value
+  airport = airport.toUpperCase()
+
+  $.ajax({
+    type: 'GET',
+    url: 'http://asanio.alwaysdata.net/index.php',
+    data: { request: "get_url_of_vfr_chart", airport: airport },
+    dataType: 'json',
+    statusCode: {
+      204: function() {
+        document.getElementById('zone_vfr_charts').style.visibility = 'hidden'
+      }
+    },
+    success: function (result) {
+      if(result){
+
+        chrome.tabs.create({ url: result });
+        
       }
   
     }
@@ -61,16 +150,12 @@ function get_datalist_ifr_charts_of_airport(icao_airport){
 }
 
 function open_link_of_sia(){
+
     var name_of_chart = document.getElementById('chart_selected').value
-    
-    $.ajax({
-      type: 'GET',
-      url: 'http://asanio.alwaysdata.net/index.php',
-      data: { request: "get_url_of_chart", charts: name_of_chart },
-      dataType: 'json',
-      success: function (result) {
-          chrome.tabs.create({ url: result });
-      }
-    });
+    console.log(name_of_chart)
+    const url = $('#list_ifr_chart').find('option[value="' + name_of_chart + '"]').attr('id');
+    console.log(url)
+    chrome.tabs.create({ url: url });
+
   
   }
